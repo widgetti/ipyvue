781dfbad1c2e78da2dad5866b13b92104afff2fc
12rambau <pierrick.rambaud49@gmail.com>
2021-09-02 11:25:40 +0000

    style: leave unrelated code untouch

--- ipyvue/VueWidget.py
+++ ipyvue/VueWidget.py
@@ -1,9 +1,8 @@
-from traitlets import Unicode, Instance, Union, List, Any, Dict
+from traitlets import (Unicode, Instance, Union, List, Any, Dict)
 from ipywidgets import DOMWidget
-from ipywidgets.widgets.widget import widget_serialization, CallbackDispatcher
+from ipywidgets.widgets.widget import (widget_serialization, CallbackDispatcher)
 from ._version import semver
 from .ForceLoad import force_load_instance
-
 
 class ClassList:
     def __init__(self, obj):
@@ -72,26 +71,20 @@ class ClassList:
         self.obj.class_ = " ".join(dst_classes)
 
 
-
 class Events(object):
     def __init__(self, **kwargs):
         self._event_handlers_map = {}
         self.on_msg(self._handle_event)
 
     def on_event(self, event_and_modifiers, callback, remove=False):
-        new_event = event_and_modifiers.split(".")[0]
-        for existing_event in [
-            event
-            for event in self._event_handlers_map.keys()
-            if event == new_event or event.startswith(new_event + ".")
-        ]:
+        new_event = event_and_modifiers.split('.')[0]
+        for existing_event in [event for event in self._event_handlers_map.keys()
+                               if event == new_event or event.startswith(new_event + '.')]:
             del self._event_handlers_map[existing_event]
 
         self._event_handlers_map[event_and_modifiers] = CallbackDispatcher()
 
-        self._event_handlers_map[event_and_modifiers].register_callback(
-            callback, remove=remove
-        )
+        self._event_handlers_map[event_and_modifiers].register_callback(callback, remove=remove)
 
         if remove and not self._event_handlers_map[event_and_modifiers].callbacks:
             del self._event_handlers_map[event_and_modifiers]
@@ -113,31 +106,30 @@ class VueWidget(DOMWidget, Events):
 
     # Force the loading of jupyter-vue before dependent extensions when in a static context (embed,
     # voila)
-    _jupyter_vue = Any(force_load_instance, read_only=True).tag(
-        sync=True, **widget_serialization
-    )
+    _jupyter_vue = Any(force_load_instance, read_only=True).tag(sync=True, **widget_serialization)
 
-    _model_name = Unicode("VueModel").tag(sync=True)
+    _model_name = Unicode('VueModel').tag(sync=True)
 
-    _view_name = Unicode("VueView").tag(sync=True)
+    _view_name = Unicode('VueView').tag(sync=True)
 
-    _view_module = Unicode("jupyter-vue").tag(sync=True)
+    _view_module = Unicode('jupyter-vue').tag(sync=True)
 
-    _model_module = Unicode("jupyter-vue").tag(sync=True)
+    _model_module = Unicode('jupyter-vue').tag(sync=True)
 
     _view_module_version = Unicode(semver).tag(sync=True)
 
     _model_module_version = Unicode(semver).tag(sync=True)
 
-    children = List(Union([Instance(DOMWidget), Unicode()])).tag(
-        sync=True, **widget_serialization
-    )
+    children = List(Union([
+        Instance(DOMWidget),
+        Unicode()
+    ])).tag(sync=True, **widget_serialization)
 
     slot = Unicode(None, allow_none=True).tag(sync=True)
 
     _events = List(Unicode()).tag(sync=True)
 
-    v_model = Any("!!disabled!!", allow_none=True).tag(sync=True)
+    v_model = Any('!!disabled!!', allow_none=True).tag(sync=True)
 
     style_ = Unicode(None, allow_none=True).tag(sync=True)
 
@@ -148,13 +140,15 @@ class VueWidget(DOMWidget, Events):
     v_slots = List(Dict()).tag(sync=True, **widget_serialization)
 
     v_on = Unicode(None, allow_none=True).tag(sync=True)
-
+    
+    
     def __init__(self, **kwargs):
 
         self.class_list = ClassList(self)
 
         super().__init__(**kwargs)
 
+        
     def show(self):
         """Make the widget visible"""
 
@@ -167,5 +161,4 @@ class VueWidget(DOMWidget, Events):
         self.class_list.add("d-none")
 
 
-
-__all__ = ["VueWidget"]
+__all__ = ['VueWidget']
