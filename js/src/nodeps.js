6e871d7250efc230d76f5aa3d56cf26a3451bbb3
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2021-10-06 19:16:35 +0200

    fix: add missing export of TemplateModel to nodeps
    Fixes voila-dashboards/voila-vuetify#49

--- js/src/nodeps.js
+++ js/src/nodeps.js
@@ -3,6 +3,7 @@ export { VueModel } from './VueModel';
 export { VueTemplateModel } from './VueTemplateModel';
 export { VueView, createViewContext } from './VueView';
 export { HtmlModel } from './Html';
+export { TemplateModel } from './Template';
 export { ForceLoadModel } from './ForceLoad';
 export { vueRender } from './VueRenderer';
 export { VueComponentModel } from './VueComponentModel';
