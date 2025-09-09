d85f3598850af6f7cf165f217fb275e6964eab47
Maarten A. Breddels <maartenbreddels@gmail.com>
2025-09-09 20:25:35 +0200

    fix: script with curly brace before component would error
    because we 'eval' everything from the first curly brace, but
    that curly brace in a comment. This is common when you want to add
    type checking using jsdoc, which would look like this:
    ```
    /**
     * @typedef {import('@/types/foo').Bar} Bar
     */
    
    We now evaluate more like a CommonJS module, but fall back to the old
    behaviour.
    ```

--- tests/ui/test_template.py
+++ tests/ui/test_template.py
@@ -70,3 +70,74 @@ def test_template_custom_event(solara_test, page_session: playwright.sync_api.Pa
     page_session.locator("text=Click Me").click()
     page_session.locator("text=Clicked").wait_for()
     assert last_event_data == "not-an-event-object"
+
+
+class MyTemplateScript(vue.VueTemplate):
+    clicks = Int(0).tag(sync=True)
+
+    @default("template")
+    def _default_vue_template(self):
+        return """
+        <template>
+            <div @click="click">Clicked {{clicks}}</div>
+        </template>
+        <script>
+            /* test { and } in a comment, which fails in ipyvue <= 1.12.2 */
+            module.exports = {
+                methods: {
+                    click() {
+                        this.clicks += 1
+                    }
+                }
+            }
+        </script>
+        """
+
+
+class MyTemplateScriptOld(vue.VueTemplate):
+    clicks = Int(0).tag(sync=True)
+
+    @default("template")
+    def _default_vue_template(self):
+        return """
+        <template>
+            <div @click="click">Clicked {{clicks}}</div>
+        </template>
+        <script>
+            /* in ipyvue <= 1.12.2, you could put anything before
+               the first curly open */
+            foo.bar = {
+                methods: {
+                    click() {
+                        this.clicks += 1
+                    }
+                }
+            }
+        </script>
+        """
+
+
+@pytest.mark.parametrize(
+    "template_class_name", ["MyTemplateScript", "MyTemplateScriptOld"]
+)
+def test_template_script(
+    ipywidgets_runner, page_session: playwright.sync_api.Page, template_class_name
+):
+    def kernel_code(template_class_name=template_class_name):
+        # this import is need so when this code executes in the kernel,
+        # the class is imported
+        from test_template import MyTemplateScript, MyTemplateScriptOld
+
+        template_class = {
+            "MyTemplateScript": MyTemplateScript,
+            "MyTemplateScriptOld": MyTemplateScriptOld,
+        }[template_class_name]
+
+        widget = template_class()
+        display(widget)
+
+    ipywidgets_runner(kernel_code, {"template_class_name": template_class_name})
+    widget = page_session.locator("text=Clicked 0")
+    widget.wait_for()
+    widget.click()
+    page_session.locator("text=Clicked 1").wait_for()
